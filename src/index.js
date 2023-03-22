ee1b87d6afeda8a7c34c84451ef7a27608e14150
Lenon <lenoninformatica@gmail.com>
2023-03-22 20:47:40 -0300

    Requisito 2 rota talker/:id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const readFile = require('./utils/fileRead');
+// const talkers = require('./talker.json');
 
 const app = express();
 app.use(express.json());
@@ -7,12 +8,26 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-app.get('/talker', async (request, response) => {
+app.get('/talker', async (_request, response) => {
   const leitura = await readFile();
   if (leitura.length === 0) {
     return response.status(HTTP_OK_STATUS).json([]);
   }
   return response.status(HTTP_OK_STATUS).json(leitura);
+});
+
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const leitura = await readFile();
+  const [talkerInfo] = leitura.filter((objInfo) => {
+    if (objInfo.id == id) {
+      return objInfo;
+    }
+  });  
+  if (!talkerInfo) {
+    return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });    
+  }
+  return response.status(200).json(talkerInfo);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
